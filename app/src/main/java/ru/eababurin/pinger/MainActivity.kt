a08119223eb59cf4cb8b8f8affc3b450660babca
Evgenii Baburin <r3drum@yandex.ru>
2022-08-15 08:03:39 +0300

    поигрался со стилем; удалил старый фрагмент и макет

--- app/src/main/java/ru/eababurin/pinger/MainActivity.kt
+++ app/src/main/java/ru/eababurin/pinger/MainActivity.kt
@@ -1,13 +1,7 @@
 package ru.eababurin.pinger
 
-import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
